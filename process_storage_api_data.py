43b163aa9f0b0951fad2c11bae55828927be9f99
oliveiraleo <oliveiraleo@users.noreply.github.com>
2022-11-23 13:56:13 -0300

    Adds the functionality to process MQTT data too

--- process_storage_api_data.py
+++ process_storage_api_data.py
@@ -2,7 +2,7 @@ import json
 import csv
 import base64
 
-def get_file_binary_data(file_name):
+def get_file_data(file_name):
     with open(file_name, "r") as f:
         print("[INFO] Reading the file", file_name)
         data = f.read()
@@ -13,8 +13,8 @@ def process_raw_data(data, file_name_to_save):
     d_list = data.splitlines()
     j_list = []
     id_and_rssi_list = []
-    for elem in d_list: #filters only the JSON formatted data
-        if elem != '':
+    for elem in d_list:
+        if elem != '': #filters out the blank lines
             j_list.append(elem)
 
     header = ['id', 'GW RSSI'] #header for the CSV file
@@ -27,9 +27,18 @@ def process_raw_data(data, file_name_to_save):
 
         for elem in j_list: #filters the data of interest and writes it to the CSV file
             dct = json.loads(elem)
-            rssi = dct['result']['uplink_message']['rx_metadata'][0]['rssi']
+            try: #executes the filtering according to the Storage API data format
+                rssi = dct['result']['uplink_message']['rx_metadata'][0]['rssi']
+                id_base64 = dct['result']['uplink_message']['frm_payload']
 
-            id_base64 = dct['result']['uplink_message']['frm_payload']
+            except KeyError: #if it fails, tries to filter according to the MQTT API data format
+                rssi = dct['uplink_message']['rx_metadata'][0]['rssi']
+                id_base64 = dct['uplink_message']['frm_payload']
+
+            except json.decoder.JSONDecodeError:
+                print("[ERROR] Couldn't parse data! Please, check if you choose the correct file")
+                raise SystemExit(0)
+
             id_b64_encoded = id_base64.encode('ascii')
             id = base64.b64decode(id_b64_encoded).decode('ascii')
 
@@ -42,7 +51,7 @@ def menu():
     print("Please, give the name of the TXT file you want to read from")
     file_name = str(input("Type (or paste) it here: "))
 
-    file_data_raw = get_file_binary_data(file_name)
+    file_data_raw = get_file_data(file_name)
     process_raw_data(file_data_raw, file_name)
 
 def main():
